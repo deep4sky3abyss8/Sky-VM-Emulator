7b1e5a95330c3d29e3b2ed5359d3af804274bfb7
USERNAME <mmdmahdiqasemisa@gmail.com>
2025-05-02 19:09:04 +0430

    first backup, assembly near to finish

---
+++ simplefilereaderprogram_allinone.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#ifndef  MAX
+#define MAX 100
+#endif
+
+void read_from_file(const char *filename ){
+	FILE *file = fopen(filename , "r") ;
+	char word[MAX] ;
+	while ( fscanf(file,"%s" , word ) != EOF ){
+		printf("%s\n",word );
+	}
+	fclose(file) ;
+}
+
+int main(void) {
+	const char *filename="s.txt" ;
+	read_from_file(filename) ;
+	return 0 ;
+}
