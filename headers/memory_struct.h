590f78ff5a9a209762c6e2f54d83a286ae59ee59
mmdqsa438 <mmdmahdiqasemisa@gmail.com>
2025-06-03 02:49:26 -0700

    memory changed

--- headers/memory_struct.h
+++ headers/memory_struct.h
@@ -1,10 +1,20 @@
 #ifndef memory_struct_h
 #define memory_struct_h
 
-extern int OS_Ram[]
-extern int PR_Ram[]
-extern int Var_Mem[]
-extern int Stck_Mem[]
-extern int Eip_Mem[]
+
+extern struct memory_struct OS_Ram[] ;
+extern struct memory_struct PR_Ram[] ;
+extern int Var_Mem[] ;
+extern int Stck_Mem[] ;
+extern int Eip_Mem[] ;
+
+/*
+struct memory_struct{
+    char command[5] ;
+    int v1 ,
+        v2 ,
+        v3 ;
+};
+*/
 
 #endif
