46169aef2f26b35860c7c435f3a49cce594354f8
mmdqsa438 <mmdmahdiqasemisa@gmail.com>
2025-06-11 08:12:06 +0330

    structs to .headers

--- headers/memory_struct.h
+++ headers/memory_struct.h
@@ -1,32 +1,42 @@
 #ifndef memory_struct_h
 #define memory_struct_h
 //
-extern struct ram os_ram[] ;
-extern struct ram pr_ram[] ;
-extern struct heap_s heap ;
+//------------------------------------------------ STRUCTS ---------------
+struct regs {  // a register which is a pointer to a place in memory .
+    void * address ;
+    char type; // [ I/C/S ] -> short for int/char/str , type of address of memory , when we want to cast void reg . use it to check typeof cast .
+};
+struct ram {
+    char command[5];
+    int v1,  // [+] this part for some commands which get value , like ASSN or JUMP which get line number to jump .
+        v2,
+        v3; // be careful about datas and regs when you want to read program from file and fill ram . check commans then fill .
+};
+struct heap_s { // short for heap segment .
+    int ints[100]; // -> 100 cell for integers .
+    char chrs[100],// -> 100 cell for single characters .
+         strs[1000];//-> 1000 cell for strings .
+};
+//------------------------------------------------ GLOBALS ---------------
+struct ram os_ram[1000];
+struct ram pr_ram[1000];
+struct heap_s heap ;
 /* heap memory of programs and os . access by register pointers .
  * [+] passing address of them by assembly command : MOVE {register number} {heap index} {type:str/int/chr}
 */
-extern int stck_mem[] ,
-           os_eip ,  // point to line of ram which read .
-           pr_eip ,
-           which_ram ;
-extern struct regs registers[] ;
-/*
-struct ram {
-    char command[5];
-    int v1,
-        v2,
-        v3;
-};
-struct heap_s { // short for heap segment .
-    int ints[100];
-    char chrs[100],
-         strs[1000];
-};
-struct regs {  // a register which is a pointer to a place in memory .
-    void * address ;
-    char type[4]; // type of address of memory , when we want to cast void reg . use it to check typeof cast .
-};
-*/
+
+
+
+int stck_mem[100] , // short for stack memory segment .
+    os_eip=0 ,  // -> eip registers which tell us which line of program/os we are in .
+    pr_eip=0 ,
+    which_ram= OS ; // -> it's the global flag which tell us now we are using os ram or programs ram ?!
+//
+struct regs registers[15];
+
+
+/*read heap_M to understand about it .
+ * [+] in worse case our command has 3 argoments so we need 3 registers for handel it .
+ * [+] in C for command ASSN Rn Index Type : reg[Rn].address = &(heap
+ */
 #endif
--- main.c
+++ main.c
@@ -3,39 +3,7 @@
 #include "headers/typing-wellcome.h"
 #include "headers/machine_lan.h"
 #include "headers/disassmbly.h"
-//------------------------------------------------ STRUCTS ---------------
-struct regs {  // a register which is a pointer to a place in memory .
-    void * address ;
-    char type; // [ I/C/S ] -> short for int/char/str , type of address of memory , when we want to cast void reg . use it to check typeof cast .
-};
-struct ram {
-    char command[5];
-    int v1,  // [+] this part for some commands which get value , like ASSN or JUMP which get line number to jump .
-        v2,
-        v3; // be careful about datas and regs when you want to read program from file and fill ram . check commans then fill .
-};
-struct heap_s { // short for heap segment .
-    int ints[100]; // -> 100 cell for integers .
-    char chrs[100],// -> 100 cell for single characters .
-         strs[1000];//-> 1000 cell for strings .
-};
-//------------------------------------------------ GLOBALS ---------------
-struct ram os_ram[1000];
-struct ram pr_ram[1000];
-struct heap_s heap ;
-/* heap memory of programs and os . access by register pointers .
- * [+] passing address of them by assembly command : MOVE {register number} {heap index} {type:str/int/chr}
-*/
-int stck_mem[100] , // short for stack memory segment .
-    os_eip=0 ,  // -> eip registers which tell us which line of program/os we are in .
-    pr_eip=0 ,
-    which_ram= OS ; // -> it's the global flag which tell us now we are using os ram or programs ram ?!
-//
-struct regs registers[10];
-/*read heap_M to understand about it .
- * [+] in worse case our command has 3 argoments so we need 3 registers for handel it .
- * [+] in C for command ASSN Rn Index Type : reg[Rn].address = &(heap
- */
+#include "headers/memory_struct.h"
 //--------------------------------------------           -----------------
 int main(void) {
     // wellcome() ; // need fix from tea lib to hamda project .
