e273d3fb276f287fc45f0ee06041b4fc9bc7dda9
mmdqsa438 <mmdmahdiqasemisa@gmail.com>
2025-07-27 00:40:54 +0330

    str generator engine added

--- tools/str_generate_engine.py
+++ tools/str_generate_engine.py
@@ -1,1 +1,34 @@
-print("####### ##   ## ##   ##     ###   ###  #####   ###### ##   ## ## ###   ## ####### \n##      ##  ##   ##  ##     ####  #### ##   ## ##      ##   ## ## ####  ## ##      \n####### #####     ####      ## #### ## ####### ##      ########## ## ##  ## #####  \n     ## ##  ##     ##       ##  ##  ## ##   ## ##      ##     ## ##  ## ## ##      \n####### ##   ##    ##       ##     ## ##   ##  ###### ##     ## ##   #### ####### \n                                                                                \n")
+from colorama import Fore , init
+import random
+import os
+init()
+conted = 1 
+while conted :
+    while True :
+        try :
+            reg = int(input(Fore.YELLOW+"\n>>> Enter Register Index/Number between 0-100 : "+Fore.RESET))
+            if 0<=reg<=100 :
+                break
+            print(Fore.RED+f"\n[!] InputError  Code{random.randint(0,10000)} : Invalid input as < index >"+Fore.RESET)
+        except :
+            print(Fore.RED+f"\n[!] InputError  Code{random.randint(0,10000)} : Invalid input as < index >"+Fore.RESET)
+    string = input(Fore.YELLOW+">>> Enter Your Str ( terminate by Enter ) : "+Fore.RESET)
+    print("\n\n>>> Your Str Disassembly Printing Code\n"+Fore.GREEN)
+    # ASSN 8 Y
+    # PUTC 8
+    for letter in string :
+        print(f"ASSN {reg} {ord(letter)}")
+        print(f"PUTC {reg}")
+    print(Fore.RESET+"\n\n\n>>> Copy Your Output ...\n")
+    while True :
+        try :
+            conted = int(input(f">>> Press ...\n\n\t[ 1 ] to input again\n\t[ 0 ] to exit\n\n>>> Your Choise : "))
+            if conted==1 or conted==0 :
+                break
+            print(Fore.RED+f"\n[!] InputError  Code{random.randint(0,10000)} : Invalid input as < choise >"+Fore.RESET)
+        except :
+            print(Fore.RED+f"\n[!] InputError  Code{random.randint(0,10000)} : Invalid input as < choise >"+Fore.RESET)
+os.system("cls"or"clear")
+
+# Created by mMahdi in 7/27/2025 - 12:27
+
